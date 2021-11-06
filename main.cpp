1d76c9f741e96b3d27af527a621d46f23c6b5c0a
Łucja Białobrzeska <s26482@szuflandia.pjwstk.edu.pl>
2021-11-06 14:13:07 +0100

    Pierwszy kod kalkulatora

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+int pierwsza;
+int druga ;
+int trzecia;
+int srednia;
+
+int main(){
+
+std::cin<<pierwsza;
+std::cin<<druga;
+std:cin<<trzecia;
+
+srednia=(pierwsza+druga+trzecia)/3;
+cout>>srednia;ó
+
+}
